6b92f98e84356535dd60e65987d45685ceb505b4
GC <2845740149@qq.com>
2022-03-15 22:24:07 +0800

    First mini web page

--- Tutorial-2/tutorial2/meetups/views.py
+++ Tutorial-2/tutorial2/meetups/views.py
@@ -6,4 +6,27 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "meetups/index.html")
+    meetups = [
+        { "title": "A first meetup", "location": "New York", "slug": "a-first-meetup" },
+        { "title": "The second meetup", "location": "Paris", "slug": "a-second-meetup" },
+        { "title": "The third meetup", "location": "Shanghai", "slug": "a-third-meetup" }
+    ]
+
+    return render(request, "meetups/index.html", {
+        "meetups": meetups
+    })
+
+
+
+def meetup_details(request, meetup_slug):
+    print(meetup_slug)
+    
+    selected_meetup = {
+        "title": "A first meetup",
+        "description": "This is the first meetup"
+    }
+
+    return render(request, "meetups/meetup-details.html", {
+        "meetup_title": selected_meetup["title"],
+        "meetup_description": selected_meetup["description"]
+    })
