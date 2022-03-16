6ad38d09a2cfbeb2b14babf99622c5bcd78aca87
GC <2845740149@qq.com>
2022-03-16 14:11:19 +0800

    Templates inheritance and includes

--- Tutorial-2/tutorial2/meetups/urls.py
+++ Tutorial-2/tutorial2/meetups/urls.py
@@ -4,5 +4,5 @@ from .import views
 
 urlpatterns = [
     path('meetups/', views.index, name="all-meetups"), # our-domain.com/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name="meetup-detail") # our-domain.com/meetups/<dynamic-path-segment>
+    path('meetups/<str:meetup_slug>', views.meetup_details, name="meetup-detail") # our-domain.com/meetups/<dynamic-path-segment>
 ]
