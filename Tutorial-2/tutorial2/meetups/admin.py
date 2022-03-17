44b6b3a3dd0d3551e5476ba693fcc7ba76981036
GC <2845740149@qq.com>
2022-03-17 14:27:53 +0800

    Configuring the admin area

--- Tutorial-2/tutorial2/meetups/admin.py
+++ Tutorial-2/tutorial2/meetups/admin.py
@@ -5,4 +5,13 @@ from .models import Meetup
 
 # Register your models here.
 
-admin.site.register(Meetup) 
+
+class MeetupAdmin(admin.ModelAdmin):
+    # Control how our meetups should be presented in the admin area and how they should behave here
+    list_display = ('title', 'slug')
+    list_filter = ('title',)
+    prepopulated_fields = {"slug": ('title',)}
+
+
+admin.site.register(Meetup, MeetupAdmin) 
+
