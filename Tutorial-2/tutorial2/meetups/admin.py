ffec38fa7152d7b377d8cdb3410d39b97f1a09ab
GC <2845740149@qq.com>
2022-03-18 14:53:03 +0800

    One-to-Many and Many-to-Many relationships in models.py

--- Tutorial-2/tutorial2/meetups/admin.py
+++ Tutorial-2/tutorial2/meetups/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Location, Participant
 
 
 # Register your models here.
@@ -8,10 +8,12 @@ from .models import Meetup
 
 class MeetupAdmin(admin.ModelAdmin):
     # Control how our meetups should be presented in the admin area and how they should behave here
-    list_display = ('title', 'slug')
-    list_filter = ('title',)
+    list_display = ('title', 'date', 'location')
+    list_filter = ('location', 'date')
     prepopulated_fields = {"slug": ('title',)}
 
 
 admin.site.register(Meetup, MeetupAdmin) 
+admin.site.register(Location)
+admin.site.register(Participant)
 
